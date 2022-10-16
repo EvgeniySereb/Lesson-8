c8ed290c54f71a600e684c0b915b9cc55f8aff5b
Evgeniy <jenyasereb95@gmail.com>
2022-10-16 11:39:30 +0500

    54

---
+++ Task54.cs
@@ -0,0 +1,74 @@
+﻿
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+
+ void ChangeArray(int [,] arr )
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int z = 0; z < arr.GetLength(1) - 1; z++)
+            {
+                if (arr[i, z] < arr[i, z + 1])
+                {
+                    
+                    int temp = arr[i, z + 1];
+                    arr[i, z + 1] = arr[i, z];
+                    arr[i, z] = temp;
+                }
+            }
+        }
+    }
+}
+            
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+
+PrintArray(array);
+int [,] newarray = array;
+ChangeArray(newarray);
+PrintArray(newarray);
